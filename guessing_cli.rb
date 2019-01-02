7728a999b664e9e3f77401bc45b561a0be1908db
enchanting-page-2316 <yannick.hoegerle@gmail.com>
2019-01-02 22:56:46 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,27 @@
 # Code your solution here!
+def exit
+  puts "Goodbye!"
+end
+
+def generate_rand_num
+  rand(1..6)
+end
+
+def run_guessing_game
+  user_guess = ""
+  until user_guess == "exit"
+    puts "Guess a number between 1 and 6."
+    user_guess = gets.chomp
+    computer_guess = generate_rand_num
+    if user_guess == "exit"
+      exit
+      break
+    elsif user_guess.to_i == computer_guess
+      puts "You guessed the correct number!"
+    elsif user_guess.to_i != computer_guess
+      puts "The computer guessed #{computer_guess}."
+    else
+      puts "Invalid input, please try again"
+    end
+  end
+end
